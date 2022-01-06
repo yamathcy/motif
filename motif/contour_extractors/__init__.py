2c6ea7e4b0599b669d5d77226b0cce569e7fb373
yamathcy <s1921652@s.tsukuba.ac.jp>
2022-01-06 23:02:28 +0900

    add utils in __all__ in init.py at contour_extractors

--- motif/contour_extractors/__init__.py
+++ motif/contour_extractors/__init__.py
@@ -4,4 +4,4 @@ from .hll import HLL
 from .salamon import Salamon
 from .peak_stream import PeakStream
 from .deepsal import DeepSal
-__all__ = ['hll', 'peak_stream', 'salamon', 'deepsal']
+__all__ = ['hll', 'peak_stream', 'salamon', 'deepsal', 'utils']
